4394c2f5b0de9215a0a26d7c9e858b90ac020a24
Xavier Olmstead <xavier.olmstead@mymail.champlain.edu>
2024-03-07 13:43:33 -0500

    Worked on board calculation

--- chess-bot/chess-simulator.cpp
+++ chess-bot/chess-simulator.cpp
@@ -5,6 +5,11 @@
 #include <random>
 using namespace ChessSimulator;
 
+int getBoardScore(chess::Board& board, bool isWhite);
+int getMaterialScore(const chess::Board& board, bool isWhite);
+int getMobilityScore(const chess::Board& board, bool isWhite);
+int minmaxMove(chess::Movelist moves, int depth, bool isMaximizing, bool isWhite, std::string fen, chess::Move& bestMove);
+
 std::string ChessSimulator::Move(std::string fen) {
   // create your board based on the board string following the FEN notation
   // search for the best move using minimax / monte carlo tree search /
@@ -24,6 +29,124 @@ std::string ChessSimulator::Move(std::string fen) {
   std::random_device rd;
   std::mt19937 gen(rd());
   std::uniform_int_distribution<> dist(0, moves.size() - 1);
-  auto move = moves[dist(gen)];
+  chess::Move move;
+
+  minmaxMove(moves, 1, true, true, fen, move);
+
   return chess::uci::moveToUci(move);
+}
+
+int minmaxMove(chess::Movelist moves, int depth, bool isMaximizing, bool isWhite, std::string fen, chess::Move& bestMove)
+{
+    chess::Board board(fen);
+
+    if (depth == 0)
+    {
+        //determine the board score
+
+        //1. compare materials
+
+        int materialScore = getBoardScore(board, isWhite);
+
+        return materialScore;
+    }
+
+    if (isMaximizing)
+    {
+        int maxValue = -INFINITY;
+        for (chess::Move move : moves)
+        {
+            chess::Board tempBoard(board);
+            std::string uciMove = chess::uci::moveToUci(move);
+
+            tempBoard.makeMove(move);
+            chess::Movelist newMoves;
+            chess::movegen::legalmoves(newMoves, tempBoard);
+
+            int evaluation = minmaxMove(newMoves, depth - 1, false, !isWhite, tempBoard.getFen(), bestMove);
+            maxValue = fmax(maxValue, evaluation);
+
+            if (maxValue == evaluation)
+            {
+                bestMove = move;
+            }
+        }
+
+        return maxValue;
+    }
+    else
+    {
+        int minValue = INFINITY;
+
+        for (chess::Move move : moves)
+        {
+            chess::Board tempBoard(board);
+            std::string uciMove = chess::uci::moveToUci(move);
+
+            tempBoard.makeMove(move);
+            chess::Movelist newMoves;
+            chess::movegen::legalmoves(newMoves, tempBoard);
+
+            int evaluation = minmaxMove(newMoves, depth - 1, true, !isWhite, tempBoard.getFen(), bestMove);
+            minValue = fmin(minValue, evaluation);
+
+            if (minValue == evaluation)
+            {
+                bestMove = move;
+            }
+        }
+
+        return minValue;
+    }
+}
+
+int getBoardScore(chess::Board& board, bool isWhite)
+{
+    //determining the score of the board based on materials
+    int materialScore = getMaterialScore(board, isWhite);
+
+    //2. compare development
+
+    //3. compare central control
+
+    //4. king safety
+
+    //5.compare pawn structure
+
+    return materialScore;
+}
+
+int getMaterialScore(const chess::Board& board, bool isWhite)
+{
+    int materialScore = 0;
+
+    if (isWhite)
+    {
+        materialScore += 999 * (board.pieces(chess::PieceType::KING, chess::Color::WHITE).count() - board.pieces(chess::PieceType::KING, chess::Color::BLACK).count());
+        materialScore += 9 * (board.pieces(chess::PieceType::QUEEN, chess::Color::WHITE).count() - board.pieces(chess::PieceType::QUEEN, chess::Color::BLACK).count());
+        materialScore += 5 * (board.pieces(chess::PieceType::ROOK, chess::Color::WHITE).count() - board.pieces(chess::PieceType::ROOK, chess::Color::BLACK).count());
+        materialScore += 3 * (board.pieces(chess::PieceType::BISHOP, chess::Color::WHITE).count() - board.pieces(chess::PieceType::BISHOP, chess::Color::BLACK).count());
+        materialScore += 3 * (board.pieces(chess::PieceType::KNIGHT, chess::Color::WHITE).count() - board.pieces(chess::PieceType::KNIGHT, chess::Color::BLACK).count());
+        materialScore += 1 * (board.pieces(chess::PieceType::PAWN, chess::Color::WHITE).count() - board.pieces(chess::PieceType::PAWN, chess::Color::BLACK).count());
+    }
+    else
+    {
+        materialScore += 999 * (board.pieces(chess::PieceType::KING, chess::Color::BLACK).count() - board.pieces(chess::PieceType::KING, chess::Color::WHITE).count());
+        materialScore += 9 * (board.pieces(chess::PieceType::QUEEN, chess::Color::BLACK).count() - board.pieces(chess::PieceType::QUEEN, chess::Color::WHITE).count());
+        materialScore += 5 * (board.pieces(chess::PieceType::ROOK, chess::Color::BLACK).count() - board.pieces(chess::PieceType::ROOK, chess::Color::WHITE).count());
+        materialScore += 3 * (board.pieces(chess::PieceType::BISHOP, chess::Color::BLACK).count() - board.pieces(chess::PieceType::BISHOP, chess::Color::WHITE).count());
+        materialScore += 3 * (board.pieces(chess::PieceType::KNIGHT, chess::Color::BLACK).count() - board.pieces(chess::PieceType::KNIGHT, chess::Color::WHITE).count());
+        materialScore += 1 * (board.pieces(chess::PieceType::PAWN, chess::Color::BLACK).count() - board.pieces(chess::PieceType::PAWN, chess::Color::WHITE).count());
+    }
+
+    return materialScore;
+}
+
+int getMobilityScore(const chess::Board& board, bool isWhite)
+{
+    auto fen = board.getFen();
+
+
+
+    return -1;
 }
